264cf08406e5e83c150559bbcecfd4117121fc7d
JunbeomKim <kimjunbum00805@gmail.com>
2023-11-21 00:17:47 -0500

    section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,11 +1,10 @@
-using System;
-using System.Threading.Tasks;
-
 namespace API.Entities
 {
     public class AppUser
     {   
         public int Id { get; set; }
         public string UserName {get;set;}
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,15 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using System.Text;
+using API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors();
+builder.Services.AddApplicationService(builder.Configuration);
+builder.Services.AddIdentityService(builder.Configuration);
 
 var app = builder.Build();
 
@@ -19,6 +17,7 @@ app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("ht
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
